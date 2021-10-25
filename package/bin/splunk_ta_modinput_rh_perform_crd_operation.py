35b188779d46ccd14cfd5d2aac13e677e704282f
Ayush Patel <84839562+ayushp-crest@users.noreply.github.com>
2021-10-25 13:30:33 +0530

    feat: Extended operations of Modinput Test TA (#202)
    * Added update_file, delete_dir & is_dir operations
    
    * Added execute command functionality
    
    * feat: extended CRUD operations

--- package/bin/splunk_ta_modinput_rh_perform_crd_operation.py
+++ package/bin/splunk_ta_modinput_rh_perform_crd_operation.py
@@ -8,6 +8,8 @@ import splunk.admin as admin
 import os
 import shutil
 import base64
+import subprocess
+
 class Splunk_TA_Modinput_Test(admin.MConfigHandler):
     def setup(self):
         if self.customAction == 'delete':
@@ -18,6 +20,15 @@ class Splunk_TA_Modinput_Test(admin.MConfigHandler):
         elif self.customAction == "create":
             self.supportedArgs.addReqArg('file_path')
             self.supportedArgs.addReqArg('data')
+        elif self.customAction == "update":
+            self.supportedArgs.addReqArg('file_path')
+            self.supportedArgs.addReqArg('data')
+        elif self.customAction == 'delete_dir':
+            self.supportedArgs.addReqArg('dir_path')
+        elif self.customAction == 'is_dir':
+            self.supportedArgs.addReqArg('dir_path')
+        elif self.customAction == 'execute':
+            self.supportedArgs.addReqArg('command')
         return
 
     def handleCustom(self, confInfo):
@@ -27,6 +38,14 @@ class Splunk_TA_Modinput_Test(admin.MConfigHandler):
             self.read_file(confInfo)
         elif self.customAction == 'create':
             self.create_file(confInfo)
+        elif self.customAction == 'update':
+            self.update_file(confInfo)
+        elif self.customAction == 'delete_dir':
+            self.delete_dir(confInfo)
+        elif self.customAction == 'is_dir':
+            self.is_dir(confInfo)
+        elif self.customAction == 'execute':
+            self.execute_command(confInfo)
 
     def delete_file(self, confInfo):
         file_path = self.callerArgs.data['file_path'][0]
@@ -63,6 +82,42 @@ class Splunk_TA_Modinput_Test(admin.MConfigHandler):
         with open(conf_file_path, "w") as conf_file_obj:
                 conf_file_obj.write(conf_file_data)
 
+    def update_file(self, confInfo):
+        conf_file_path = self.callerArgs.data['file_path'][0]
+        conf_file_data = self.callerArgs.data['data'][0]
+        path_to_local = "/".join(conf_file_path.split("/")[:-1])
+        if not os.path.exists(path_to_local):
+            os.makedirs(path_to_local)
+        with open(conf_file_path, "a") as conf_file_obj:
+                conf_file_obj.write(conf_file_data)
+
+    def is_dir(self, confInfo):
+        dir_path = self.callerArgs.data['dir_path'][0]
+        if os.path.exists(dir_path) and os.path.isdir(dir_path):
+            confInfo['success_message'] = ('positive_response', '{} is a directory.'.format(dir_path))
+        else:
+            confInfo['error_message'] = ('negative_response', '{} is not a direcotry'.format(dir_path))
+
+    def delete_dir(self, confInfo):
+        dir_path = self.callerArgs.data['dir_path'][0]
+        if os.path.exists(dir_path):
+            if os.path.isdir(dir_path):
+                shutil.rmtree(dir_path)
+                confInfo['success_message'] = ('success_message', 'Directory {} successfully deleted'.format(dir_path))
+            else:
+                confInfo['error_message'] = ('delete_error_message', '{} is not a direcotry'.format(dir_path))
+
+        else:
+            confInfo['error_message'] = ('delete_error_message', 'Directory {} not found'.format(dir_path))
+
+    def execute_command(self, confInfo):
+        command = self.callerArgs.data['command'][0]
+        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        p.wait()
+        lines = p.stdout.readlines()
+        output = []
+        [output.append(str(line, "utf-8")) for line in lines]
+        confInfo['success_message'] = ('output', output)
 
 if __name__ == "__main__":
     admin.init(Splunk_TA_Modinput_Test, admin.CONTEXT_APP_AND_USER)
